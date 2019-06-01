714fac37d488234fe7cd46f90642b339871ae94e
niklas <23435250+Biospheere@users.noreply.github.com>
2019-06-01 18:45:25 +0200

    Track played games when guild is ready

--- src/main/java/de/biosphere/promcord/handler/guild/UserGameListener.java
+++ src/main/java/de/biosphere/promcord/handler/guild/UserGameListener.java
@@ -1,9 +1,13 @@
 package de.biosphere.promcord.handler.guild;
 
 import io.prometheus.client.Gauge;
+import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.core.events.user.update.UserUpdateGameEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.HashMap;
 
 public class UserGameListener extends ListenerAdapter {
 
@@ -22,5 +26,14 @@ public class UserGameListener extends ListenerAdapter {
             final long count = event.getGuild().getMembers().stream().filter(member -> (member.getGame() != null && member.getGame().isRich() && member.getGame().getName().equals(event.getNewGame().getName()))).count();
             game_count.labels(event.getGuild().getId(), event.getNewGame().getName()).set(count);
         }
+    }
+
+    @Override
+    public void onGuildReady(GuildReadyEvent event) {
+        final HashMap<String, Integer> gameMap = new HashMap<>();
+        event.getGuild().getMembers().stream().filter(member -> !member.getOnlineStatus().equals(OnlineStatus.OFFLINE) && member.getGame() != null && member.getGame().isRich() && member.getGame().getType().equals(Game.GameType.DEFAULT)).forEach(member -> {
+            gameMap.compute(member.getGame().getName(), (key, val) -> val == null ? 1 : val + 1);
+        });
+        gameMap.forEach((key, value) -> game_count.labels(event.getGuild().getId(), key).set(value));
     }
 }
