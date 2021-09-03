dd38c1307038cc7ed0158a6b52dccb66e13c28dd
Niklas Marion <niklas.marion@gmx.de>
2021-09-03 09:18:07 +0200

    Revert "Rssnyder master"

--- src/main/java/de/biosphere/promcord/handler/message/MessageRecieverListener.java
+++ src/main/java/de/biosphere/promcord/handler/message/MessageRecieverListener.java
@@ -2,8 +2,6 @@ package de.biosphere.promcord.handler.message;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import de.biosphere.promcord.Configuration;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import net.dv8tion.jda.api.entities.Message;
@@ -28,24 +26,17 @@ public class MessageRecieverListener extends ListenerAdapter {
     private final Gauge msg_word_count;
     private final Gauge toxicityScore;
     private final String perspectivePayload;
-    private final boolean track_names;
 
     public MessageRecieverListener() {
-        String[] tags_default = { "guild", "channel", "user" };
-        String[] tags_names = { "guild", "channel", "channelName", "user", "name" };
-        track_names = (Configuration.TRACK_NAMES != null && Configuration.TRACK_NAMES.equalsIgnoreCase("true"));
-        msg_count = Counter.build().name("msg_count").help("Count of messages")
-                .labelNames(track_names ? tags_names : tags_default).register();
+        String[] tags = { "guild", "channel", "user" };
+        msg_count = Counter.build().name("msg_count").help("Count of messages").labelNames(tags).register();
         msg_emote_count = Counter.build().name("msg_emote_count").help("Count of emotes in messages")
-                .labelNames(track_names ? new String[] { "guild", "channel", "channelName", "user", "name", "emote" }
-                        : new String[] { "guild", "channel", "user", "emote" })
+                .labelNames("guild", "channel", "user", "emote").register();
+        msg_length = Gauge.build().name("msg_length").help("Length of messages").labelNames(tags).register();
+        msg_word_count = Gauge.build().name("msg_word_count").help("Count of words in messages").labelNames(tags)
                 .register();
-        msg_length = Gauge.build().name("msg_length").help("Length of messages")
-                .labelNames(track_names ? tags_names : tags_default).register();
-        msg_word_count = Gauge.build().name("msg_word_count").help("Count of words in messages")
-                .labelNames(track_names ? tags_names : tags_default).register();
-        toxicityScore = Gauge.build().name("toxicity_score").help("ToxicityScore of a message")
-                .labelNames(track_names ? tags_names : tags_default).register();
+        toxicityScore = Gauge.build().name("toxicity_score").help("ToxicityScore of a message").labelNames(tags)
+                .register();
         perspectivePayload = "{\n" + "  \"comment\": {\n" + "    \"text\": \"message\"\n" + "  },\n"
                 + "  \"languages\": [\n" + "    \"de\"\n" + "  ],\n" + "  \"requestedAttributes\": {\n"
                 + "    \"TOXICITY\": {}\n" + "  }\n" + "}";
@@ -66,14 +57,7 @@ public class MessageRecieverListener extends ListenerAdapter {
         recordMessageWordCount(channel, user, event.getMessage().getContentDisplay().split(" ").length);
 
         event.getMessage().getEmotesBag().forEach(emote -> {
-            if (track_names) {
-                msg_emote_count.labels(channel.getGuild().getId(), channel.getId(), channel.getName(), user.getId(),
-                        user.getName(), emote.getName()).inc();
-            } else {
-                msg_emote_count.labels(channel.getGuild().getId(), channel.getId(), user.getId(), emote.getName())
-                        .inc();
-            }
-
+            msg_emote_count.labels(channel.getGuild().getId(), channel.getId(), user.getId(), emote.getName()).inc();
         });
 
         if (System.getenv("PERSPECTIVE_KEY") != null) {
@@ -82,33 +66,15 @@ public class MessageRecieverListener extends ListenerAdapter {
     }
 
     private void recordMessageWordCount(final TextChannel channel, final User user, final int length) {
-        if (track_names) {
-            msg_word_count.labels(channel.getGuild().getId(), channel.getId(), channel.getName(), user.getId(),
-                    user.getName()).set(length);
-        } else {
-            msg_word_count.labels(channel.getGuild().getId(), channel.getId(), user.getId()).set(length);
-        }
-
+        msg_word_count.labels(channel.getGuild().getId(), channel.getId(), user.getId()).set(length);
     }
 
     private void recordMessageLength(final TextChannel channel, final User user, final int length) {
-        if (track_names) {
-            msg_length.labels(channel.getGuild().getId(), channel.getId(), channel.getName(), user.getId(),
-                    user.getName()).set(length);
-        } else {
-            msg_length.labels(channel.getGuild().getId(), channel.getId(), user.getId()).set(length);
-        }
-
+        msg_length.labels(channel.getGuild().getId(), channel.getId(), user.getId()).set(length);
     }
 
     private void recordMessageCount(final TextChannel channel, final User user) {
-        if (track_names) {
-            msg_count.labels(channel.getGuild().getId(), channel.getId(), channel.getName(), user.getId(),
-                    user.getName()).inc();
-        } else {
-            msg_count.labels(channel.getGuild().getId(), channel.getId(), user.getId()).inc();
-        }
-
+        msg_count.labels(channel.getGuild().getId(), channel.getId(), user.getId()).inc();
     }
 
     private void recordToxicityScore(final Message message) {
